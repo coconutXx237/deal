fb423d96fa89ca97f623268713b7c2aad4c987d8
Oleg Klimkin <oleg402@bk.ru>
2023-02-11 01:06:37 +0400

    added final functionality

--- src/test/java/ru/klimkin/deal/service/impl/DealHandleServiceImplTest.java
+++ src/test/java/ru/klimkin/deal/service/impl/DealHandleServiceImplTest.java
@@ -16,10 +16,8 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyBoolean;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class DealHandleServiceImplTest {
